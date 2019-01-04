67edca0011a5f85f8dc891b6a3d3e0a1d8c6812d
Larry Garfield <larry@garfieldtech.com>
2019-01-04 15:29:15 -0600

    Use JS string templates.

--- nodejs/index.js
+++ nodejs/index.js
@@ -6,7 +6,16 @@ var config = require("platformsh").config();
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.end("<html><head><title>Hello node</title></head><body><h1><img src='public/js.png'>Hello Node</h1><h3>Platform configuration:</h3><pre>"+JSON.stringify(config, null, 4) + "</pre></body></html>");
+  response.end(`
+<html>
+<head>
+<title>Platform.sh Node.js service examples</title>
+</head>
+<body>
+<h1><img src='public/js.png'>Hello Node</h1>
+<h3>Platform configuration:</h3>
+<pre>${JSON.stringify(config, null, 4)}</pre></body>
+</html>`);
 });
 
 server.listen(config.port);
