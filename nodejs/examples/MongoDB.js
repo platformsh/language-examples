2ac4686ac8c9cfe347b9130531e93ad5af8ccdd6
Larry Garfield <larry@garfieldtech.com>
2019-01-04 17:37:14 -0600

    Fix module syntax for MongoDB.

--- nodejs/examples/MongoDB.js
+++ nodejs/examples/MongoDB.js
@@ -1,15 +1,15 @@
 const config = require("platformsh").config();
 
-export function relationships() {
+exports.relationships = () => {
     const credentials = config.relationships.mongodb[0];
 
     return JSON.stringify(credentials);
-}
+};
 
-export async function run() {
+exports.run = async function() {
 
     // Do various mongoDB calls, using await on them.
     // Then construct and return a string of output at the end.
 
     await "Some output";
-}
+};
