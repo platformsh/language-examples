646bb64b3e5c88c47938a338239c810c0e13ceef
Larry Garfield <larry@garfieldtech.com>
2019-01-07 13:08:53 -0600

    Rewrite Mongo example for Node.js.

--- nodejs/examples/MongoDB.js
+++ nodejs/examples/MongoDB.js
@@ -11,6 +11,44 @@ exports.run = async function() {
     const credentials = config.relationships.mongodb[0];
     let mongoose = require('mongoose');
 
+    const MongoClient = require('mongodb').MongoClient;
+
+    const connectionString = `'mongodb://${credentials["username"]}:${credentials["password"]}@${credentials["host"]}:${credentials["port"]}`;
+
+    var client = await MongoClient.connect(connectionString, { useNewUrlParser: true });
+
+    let db = client.db(credentials["path"]);
+
+    let collection = db.collection("startrek");
+
+    const documents = [
+        {'name': 'James Kirk', 'rank': 'Admiral'},
+        {'name': 'Jean-Luc Picard', 'rank': 'Captain'},
+        {'name': 'Benjamin Sisko', 'rank': 'Prophet'},
+        {'name': 'Katheryn Janeway', 'rank': 'Captain'},
+    ];
+
+    await collection.insert(documents, {w: 1});
+
+    let result = await collection.find({rank:"Captain"}).toArray();
+
+    let output = '';
+
+    output += `<table>
+<thead>
+<tr><th>Name</th><th>Rank</th></tr>
+</thead>
+<tbody>`;
+
+    result.forEach((key) => {
+        output += `<tr><td>${result[key].name}</td><td>${result[key].rank}</td></tr>\n`;
+    });
+
+    output += `</tbody>\n</table>\n`;
+
+    return output;
+
+    /*
     // Mongoose connection to MongoDB
     mongoose.connect(`'mongodb://${credentials["username"]}:${credentials['password']}@${credentials['host']}:${credentials['port']}/${credentials['path']}`, function (err) {
         if (err) {
@@ -33,6 +71,7 @@ exports.run = async function() {
     let things = await Thing.find();
 
     console.debug(things);
+    */
     
-    return "Some output";
+    //return "Some output";
 };
