fc9245aeffb88e46bcde201359b4cc8658905114
Larry Garfield <larry@garfieldtech.com>
2019-01-07 13:14:32 -0600

    Trim outdated code.

--- nodejs/examples/MongoDB.js
+++ nodejs/examples/MongoDB.js
@@ -7,12 +7,8 @@ exports.relationship = () => {
 };
 
 exports.run = async function() {
-
     const credentials = config.relationships.mongodb[0];
-    let mongoose = require('mongoose');
-
     const MongoClient = require('mongodb').MongoClient;
-
     const connectionString = `'mongodb://${credentials["username"]}:${credentials["password"]}@${credentials["host"]}:${credentials["port"]}`;
 
     var client = await MongoClient.connect(connectionString, { useNewUrlParser: true });
@@ -47,31 +43,4 @@ exports.run = async function() {
     output += `</tbody>\n</table>\n`;
 
     return output;
-
-    /*
-    // Mongoose connection to MongoDB
-    mongoose.connect(`'mongodb://${credentials["username"]}:${credentials['password']}@${credentials['host']}:${credentials['port']}/${credentials['path']}`, function (err) {
-        if (err) {
-            if (err) console.error(err);
-        }
-    });
-    var thingSchema = new mongoose.Schema({
-        title: { type: String }
-    });
-    var Thing = mongoose.model('Thing', thingSchema);
-    var foo = new Thing({
-        title: 'Platform.sh'
-    });
-    await foo.save(function(err, foo) {
-        if (err) console.error(err);
-    });
-    // Thing.find({}, function(err, foo) {
-    //     if (err) { console.error(err) } else { resp = JSON.stringify(foo, null, 4) };
-    // });
-    let things = await Thing.find();
-
-    console.debug(things);
-    */
-    
-    //return "Some output";
 };
