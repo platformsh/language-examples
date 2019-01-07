15940005561e528a547ee535dc7ef0e619d6e38d
Larry Garfield <larry@garfieldtech.com>
2019-01-07 12:06:10 -0600

    Style the output.

--- nodejs/index.js
+++ nodejs/index.js
@@ -23,7 +23,35 @@ var server = http.createServer(async function (request, response) {
 
   response.write(`<html>
 <head>
-<title>Platform.sh Node.js service examples</title>
+    <title>Platform.sh Node.js service examples</title>
+    <style type="text/css">
+        details {
+            margin-top: 1em;
+            border: 1px solid #aaa;
+            border-radius: 4px;
+            padding: 0.5em;
+            wdith: 90%;
+        }
+
+        summary {
+            font-weight: bold;
+            margin: -.5em -.5em 0;
+            padding: .5em;
+        }
+
+        details[open] {
+            padding: .5em;
+        }
+
+        details[open] summary {
+            border-bottom: 1px solid #aaa;
+            margin-bottom: .5em;
+        }
+
+        table, table td, table th {
+            border: 1px solid black;
+        }
+    </style>
 </head>
 <body>
 <h1>Service examples for Node.js</h1>
