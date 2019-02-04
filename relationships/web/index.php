f8fa48bf74d3c41f22265375b881f1a641f72886
Larry Garfield <larry@garfieldtech.com>
2019-02-04 16:29:56 -0600

    Cache the relationships output.

--- relationships/web/index.php
+++ relationships/web/index.php
@@ -6,9 +6,11 @@ $relName = $_GET['service'];
 
 $relationships = json_decode(base64_decode(getenv('PLATFORM_RELATIONSHIPS')), TRUE);
 
-
 if (empty($relationships[$relName])) {
     return;
 }
+
+// Cache real output for 5 minutes.
+header('cache-control: public max-age=300');
 
 print \json_encode($relationships[$relName][0], JSON_PRETTY_PRINT);
