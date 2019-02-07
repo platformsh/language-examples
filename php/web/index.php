c4d4de0e1a66ac18686d8a06f7fb47dff084026c
Larry Garfield <larry@garfieldtech.com>
2019-02-07 14:57:14 -0600

    Split off paths to show source and output individually.

--- php/web/index.php
+++ php/web/index.php
@@ -3,45 +3,7 @@
 declare(strict_types=1);
 
 require __DIR__.'/../vendor/autoload.php';
-?>
-<html>
-<head>
-    <title>Platform.sh PHP service examples</title>
-    <style type="text/css">
-        details {
-            margin-top: 1em;
-            border: 1px solid #aaa;
-            border-radius: 4px;
-            padding: 0.5em;
-            width: 90%;
-        }
 
-        summary {
-            font-weight: bold;
-            margin: -.5em -.5em 0;
-            padding: .5em;
-        }
-
-        details[open] {
-            padding: .5em;
-        }
-
-        details[open] summary {
-            border-bottom: 1px solid #aaa;
-            margin-bottom: .5em;
-        }
-
-        table, table td, table th {
-            border: 1px solid black;
-        }
-
-    </style>
-</head>
-<body>
-
-<h1>Service examples for PHP</h1>
-
-<?php
 
 function capture_output(callable $callable) {
     ob_start();
@@ -51,32 +13,50 @@ function capture_output(callable $callable) {
     return $contents;
 }
 
-print_r($_SERVER);
+function handleRequest(string $path)
+{
+    $routes = routeList();
 
+    if (!isset($routes[$path])) {
+        $path = '/';
+    }
 
-$files = glob("../examples/*.php");
-foreach ($files as $filename) {
-    $name = pathinfo($filename)['filename'];
-    $source = highlight_file($filename, true);
-    $output = capture_output(function() use ($filename) {
-        include $filename;
-    });
-
-    print <<<END
-<details>
-<summary>{$name} Sample Code</summary>    
-<section>
-<h3>Source</h3>
-{$source}
-</section>
-<section>
-<h3>Output</h3>
-{$output}
-</section>
-</details>
-END;
+    print $routes[$path]();
 }
-?>
 
-</body>
-</html>
+function debug($var) : void
+{
+    $val = print_r($var, true);
+
+    print "<div style='display:none'>{$val}</div>\n";
+}
+
+function routeList() : array
+{
+    $routes = [];
+
+    $routes['/'] = function() {
+        return capture_output(function() {
+            require 'list.php';
+        });
+    };
+
+    $files = glob("../examples/*.php");
+
+    foreach ($files as $filename) {
+        $file = basename($filename, '.php');
+        $path = strtolower($file);
+        $routes['/' . $path] = function() use ($filename) {
+            // We may not want to highlight the code, depending on where it's being used.
+            return highlight_file($filename, true);
+        };
+        $routes["/{$path}/output"] = function() use ($filename) {
+            return capture_output(function() use ($filename) {
+                include $filename;
+            });
+        };
+    }
+    return $routes;
+}
+
+handleRequest($_SERVER['REQUEST_URI']);
