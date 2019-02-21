c01f5ff0064f1be0114e6c6f774fb7b59cce4475
chadcarlson <chad.carlson@platform.sh>
2019-02-21 16:09:51 -0500

    try rabbitmq

--- python/pshexs/rabbitmq.py
+++ python/pshexs/rabbitmq.py
@@ -1,7 +1,10 @@
-# import pika
+import pika
 from pshconfig import Config
 import traceback, sys
 
+def callback(body):
+    # In a real application you'd put the following in a separate script in a loop.
+    print("[x] Deploying on %s<br />\n".format(body))
 
 def test_output():
 
@@ -9,31 +12,36 @@ def test_output():
     # You can alternatively use os.environ yourself.
     config = Config()
 
-    # The 'database' relationship is generally the name of primary SQL database of an application.
-    # That's not required, but much of our default automation code assumes it.' \
-    # credentials = config.credentials('database')
+    # Get the credentials to connect to the RabbitMQ service.
+    credentials = config.credentials('rabbitmq')
 
     try:
 
-        credentials = config.credentials('rabbitmq')
+        queue_name = 'deploy_days'
 
-        return credentials
+        creds = pika.PlainCredentials(credentials['username'], credentials['password'])
+        parameters = pika.ConnectionParameters(credentials['host'], credentials['port'], credentials=creds)
 
+        # Connect to the RabbitMQ server.
+        connection = pika.SelectConnection(parameters)
+
+        channel = connection.channel()
+
+        channel.queue_declare(queue_name)
+
+        msg = channel.basic_publish(exchange='', routing_key=queue_name, body='Friday')
+
+        print('[x] Sent Friday<br/>\n')
+
+        channel.basic_consume(callback(msg.body), queue=queue_name, no_ack=True)
+
+        channel.close()
+        connection.close()
 
     except Exception as e:
         return traceback.format_exc(), sys.exc_info()[0]
 
-# from pshconfig import Config
-# import pika
-#
-#
-# # Create a new config object to ease reading the Platform.sh environment variables.
-# # You can alternatively use os.environ() yourself.
-# config = Config()
-#
-# # Get the credentials to connect to the RabbitMQ service.
-# credentials = config.redentials('rabbitmq')
-#
+
 # try:
 #     queue_name = 'deploy_days'
 # #     $queueName = 'deploy_days';
