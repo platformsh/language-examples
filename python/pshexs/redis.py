e6ea692e461a7b12b113e4a3273eca91627dda08
chadcarlson <chad.carlson@platform.sh>
2019-02-21 14:34:11 -0500

    clean up and update return format from b

--- python/pshexs/redis.py
+++ python/pshexs/redis.py
@@ -1,6 +1,5 @@
 from redis import Redis
 from pshconfig import Config
-import traceback, sys
 
 
 def test_output():
@@ -14,14 +13,8 @@ def test_output():
 
     try:
 
-        # output = str(config.relationshipsDef)
-
-        # host = '127.0.0.1'
-        # port = int(os.environ["PORT"])
-
         redis = Redis(credentials['host'], credentials['port'])
-        # redis = Redis(host, port)
-        #
+
         key = "Deploy day"
         value = "Friday"
 
@@ -31,12 +24,7 @@ def test_output():
         # Read it back
         test = redis.get(key)
 
-        return('Found value <strong>{0}</strong> for key <strong>{1}</strong>.'.format(test, key))
-        # return relationships if isinstance(relationships, str) else str(relationships)
-        # return 'Platform.sh config was properly imported'
+        return('Found value <strong>{0}</strong> for key <strong>{1}</strong>.'.format(str(test), key))
 
-    except Exception:
-        # return 'platform.sh config WAS NOT properly imported.'
-        return traceback.format_exc(), sys.exc_info()[0]
-
-
+    except Exception as e:
+        return e
