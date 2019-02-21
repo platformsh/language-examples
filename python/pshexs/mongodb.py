3140831943167b4db4d143384c0dc52e33289ad4
chadcarlson <chad.carlson@platform.sh>
2019-02-21 14:55:28 -0500

    update mongodb

--- python/pshexs/mongodb.py
+++ python/pshexs/mongodb.py
@@ -1,4 +1,4 @@
-# import pymongo
+from pymongo import MongoClient
 from pshconfig import Config
 import traceback, sys
 
@@ -10,80 +10,36 @@ def test_output():
     config = Config()
 
     # The 'database' relationship is generally the name of primary SQL database of an application.
-    # That's not required, but much of our default automation code assumes it.' \
-    # credentials = config.credentials('database')
+    # It could be anything, though, as in the case here here where it's called "mongodb".
+    credentials = config.credentials('mongodb')
 
     try:
 
-        credentials = config.credentials('mongodb')
+        server = '{}://{}:{}@{}:{}/{}'.format(
+            credentials['scheme'],
+            credentials['username'],
+            credentials['password'],
+            credentials['host'],
+            credentials['port'],
+            credentials['path']
+        )
 
-        return credentials
+        client = MongoClient(server)
 
+        collection = client.starwars
+
+        result = collection.insert_one(
+            {"name": "Rey",
+             "occupation": "Jedi"}
+        )
+
+        result_id = result.inserted_id
+
+        document = collection.find_one(
+            {"_id": result_id}
+        )
+
+        return print('Found {0} ({1})<br />\n'.format(document.name, document.occupation))
 
     except Exception as e:
         return traceback.format_exc(), sys.exc_info()[0]
-
-
-# from pshconfig import Config
-# from pymongo import MongoClient
-#
-#
-# # Create a new Config object to ease reading the Platform.sh environment variables.
-# # You can alternatively use os.environ yourself.
-# config = Config()
-#
-# # Get the credentials to connect to the Elasticsearch service
-# credentials = config.credentials('mongodb')
-#
-# try:
-#     server = '{}://{}:{}@{}:{}/{}'.format(
-#         credentials['scheme'],
-#         credentials['username'],
-#         credentials['password'],
-#         credentials['host'],
-#         credentials['port'],
-#         credentials['path']
-#     )
-#
-# #     $server = sprintf('%s://%s:%s@%s:%d/%s',
-# #         $credentials['scheme'],
-# #         $credentials['username'],
-# #         $credentials['password'],
-# #         $credentials['host'],
-# #         $credentials['port'],
-# #         $credentials['path']
-# #     );
-# #
-#     client = MongoClient(server)
-#
-# #     $client = new Client($server);
-#     collection = client.starwars
-# #     $collection = $client->main->starwars;
-#
-#     result_id = collection.insert_one({
-#         "name": "Rey",
-#         "occupation": "Jedi"
-#     }).inserted_id
-# #     $result = $collection->insertOne([
-# #         'name' => 'Rey',
-# #         'occupation' => 'Jedi',
-# #     ]);
-# #
-# #
-# #     $id = $result->getInsertedId();
-# #
-#     document = collection.find_one({
-#         "_id": result_id
-#     })
-# #     $document = $collection->findOne([
-# #         '_id' => $id,
-# #     ]);
-# #
-#     print('Found {} ({})<br />\n'.format(document.name, document.occupation))
-# #     printf("Found %s (%s)<br />\n", $document->name, $document->occupation);
-# #
-# except Exception as e:
-#     print(e)
-# # } catch (\Exception $e) {
-# #     print $e->getMessage();
-# # }
