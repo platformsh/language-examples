e33761b79ada7aef73240b4565f8c6fdc8623d59
chadcarlson <chad.carlson@platform.sh>
2019-02-21 14:29:37 -0500

    redis test

--- python/pshexs/redis.py
+++ python/pshexs/redis.py
@@ -1,39 +1,39 @@
+from redis import Redis
 from pshconfig import Config
-import traceback
-import sys
+import traceback, sys
+
 
 def test_output():
 
     # Create a new config object to ease reading the Platform.sh environment variables.
     # You can alternatively use os.environ yourself.
-    # config = Config()
+    config = Config()
 
     # Get the credentials to connect to the Redis service.
-    # credentials = config.credentials('redis')
+    credentials = config.credentials('redis')
 
     try:
 
-        config = Config()
         # output = str(config.relationshipsDef)
 
         # host = '127.0.0.1'
         # port = int(os.environ["PORT"])
 
-        # redis = Redis(credentials['host'], credentials['port'])
+        redis = Redis(credentials['host'], credentials['port'])
         # redis = Redis(host, port)
-        # #
-        # key = "Deploy day"
-        # value = "Friday"
         #
-        # # Set a value
-        # redis.set(key, value)
-        #
-        # # Read it back
-        # test = redis.get(key)
+        key = "Deploy day"
+        value = "Friday"
 
-        # return('Found value <strong>{0}</strong> for key <strong>{1}</strong>.'.format(test, key))
+        # Set a value
+        redis.set(key, value)
+
+        # Read it back
+        test = redis.get(key)
+
+        return('Found value <strong>{0}</strong> for key <strong>{1}</strong>.'.format(test, key))
         # return relationships if isinstance(relationships, str) else str(relationships)
-        return 'Platform.sh config was properly imported'
+        # return 'Platform.sh config was properly imported'
 
     except Exception:
         # return 'platform.sh config WAS NOT properly imported.'
