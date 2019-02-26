9ed7c0132e78169cf4eb73ee738084f392412b2e
chadcarlson <chad.carlson@platform.sh>
2019-02-26 15:35:09 -0600

    revert create_list()

--- python/web/index.py
+++ python/web/index.py
@@ -3,7 +3,7 @@ import flask
 import gevent.pywsgi
 
 import pshexs
-from .list import create_list
+# from .list import create_list
 
 app = flask.Flask(__name__)
 
@@ -56,80 +56,80 @@ def add_example_output_route(example):
         return "Sorry, no sample code is available."
 
 
-# def create_list():
-#     names = {
-#         "elasticsearch": "Elasticsearch",
-#         "influxdb": "InfluxDB",
-#         "memcached": "Memcached",
-#         "mongodb": "MongoDB",
-#         "mysql": "MySQL",
-#         "postgresql": "PostgreSQL",
-#         "rabbitmq": "RabbitMQ",
-#         "redis": "Redis",
-#         "solr": "Solr",
-#     }
-#     header = '<html>' \
-#              '<head>' \
-#              '<title>Platform.sh Python service examples</title>' \
-#              '<script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>' \
-#              '<style type="text/css">' \
-#              '  details {' \
-#              '      margin-top: 1em;' \
-#              '      border: 1px solid #aaa;' \
-#              '      border-radius: 4px;' \
-#              '      padding: 0.5em;' \
-#              '      width: 90%;' \
-#              '  }' \
-#              '' \
-#              '  summary {' \
-#              '      font-weight: bold;' \
-#              '      margin: -.5em -.5em 0;' \
-#              '      padding: .5em;' \
-#              '  }' \
-#              '' \
-#              '  details[open] {' \
-#              '      padding: .5em;' \
-#              '  }' \
-#              '' \
-#              '  details[open] summary {' \
-#              '      border-bottom: 1px solid #aaa;' \
-#              '      margin-bottom: .5em;' \
-#              '  }' \
-#              '' \
-#              '  table, table td, table th {' \
-#              '      border: 1px solid black;' \
-#              '  }' \
-#              '</style>' \
-#              '</head>' \
-#              '<body>' \
-#              '' \
-#              '<h1>Service examples for Python</h1>'
-#
-#     body = ''
-#     services = [service for service in dir(pshexs) if '_' not in service]
-#     for service in services:
-#         name = names[service]
-#         source = file_get_contents(service)
-#         output = getattr(getattr(pshexs, service), 'test_output')()
-#
-#         first = '<details>' \
-#                '<summary>{0} Sample Code</summary>' \
-#                '<section>' \
-#                '<h3>Source</h3>' \
-#                '<pre class="prettyprint"><code class="language-py">{1}</code></pre>' \
-#                '</section>' \
-#                '<section>' \
-#                '<h3>Output</h3>' \
-#                '{2}' \
-#                '</section>' \
-#                '</details>'.format(name, source, output)
-#
-#         body += first
-#
-#     footer = '</body>' \
-#              '</html>'
-#
-#     return header + body + footer
+def create_list():
+    names = {
+        "elasticsearch": "Elasticsearch",
+        "influxdb": "InfluxDB",
+        "memcached": "Memcached",
+        "mongodb": "MongoDB",
+        "mysql": "MySQL",
+        "postgresql": "PostgreSQL",
+        "rabbitmq": "RabbitMQ",
+        "redis": "Redis",
+        "solr": "Solr",
+    }
+    header = '<html>' \
+             '<head>' \
+             '<title>Platform.sh Python service examples</title>' \
+             '<script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>' \
+             '<style type="text/css">' \
+             '  details {' \
+             '      margin-top: 1em;' \
+             '      border: 1px solid #aaa;' \
+             '      border-radius: 4px;' \
+             '      padding: 0.5em;' \
+             '      width: 90%;' \
+             '  }' \
+             '' \
+             '  summary {' \
+             '      font-weight: bold;' \
+             '      margin: -.5em -.5em 0;' \
+             '      padding: .5em;' \
+             '  }' \
+             '' \
+             '  details[open] {' \
+             '      padding: .5em;' \
+             '  }' \
+             '' \
+             '  details[open] summary {' \
+             '      border-bottom: 1px solid #aaa;' \
+             '      margin-bottom: .5em;' \
+             '  }' \
+             '' \
+             '  table, table td, table th {' \
+             '      border: 1px solid black;' \
+             '  }' \
+             '</style>' \
+             '</head>' \
+             '<body>' \
+             '' \
+             '<h1>Service examples for Python</h1>'
+
+    body = ''
+    services = [service for service in dir(pshexs) if '_' not in service]
+    for service in services:
+        name = names[service]
+        source = file_get_contents(service)
+        output = getattr(getattr(pshexs, service), 'test_output')()
+
+        first = '<details>' \
+               '<summary>{0} Sample Code</summary>' \
+               '<section>' \
+               '<h3>Source</h3>' \
+               '<pre class="prettyprint"><code class="language-py">{1}</code></pre>' \
+               '</section>' \
+               '<section>' \
+               '<h3>Output</h3>' \
+               '{2}' \
+               '</section>' \
+               '</details>'.format(name, source, output)
+
+        body += first
+
+    footer = '</body>' \
+             '</html>'
+
+    return header + body + footer
 
 
 if __name__ == "__main__":
