d56ef6a5c571cbb1e1606eb77087856cc3580f9d
chadcarlson <chad.carlson@platform.sh>
2019-02-26 12:03:23 -0600

    create user in separate command

--- python/pshexs/influxdb.py
+++ python/pshexs/influxdb.py
@@ -25,6 +25,7 @@ def test_output():
         PASSWORD = base64.b64encode(secrets.token_bytes())
 
         client = InfluxDBClient(host=HOST, port=PORT, username=USER, password=PASSWORD, database=DATABASE)
+        client.create_user(username=USER, password=PASSWORD, admin=True)
         client.create_database(DATABASE)
 
 
