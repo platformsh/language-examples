6e1e2f551fbcfe9930fb2d3d546e3508cd1b8673
chadcarlson <chad.carlson@platform.sh>
2019-02-26 10:43:10 -0600

    include dbname in Client constructor call

--- python/pshexs/influxdb.py
+++ python/pshexs/influxdb.py
@@ -21,7 +21,7 @@ def test_output():
 
         # Connecting to the InfluxDB server. By default it has no user defined, so you will need to create it.
         # client = InfluxDBClient(credentials['host'], credentials['port'], username='deploy_user', password=password)
-        client = InfluxDBClient(credentials['host'], credentials['port'], username='root', password='root')
+        client = InfluxDBClient(credentials['host'], credentials['port'], username='root', password='root', database='deploys')
 
         # client = InfluxDBClient(credentials['host'], credentials['port'])
 
