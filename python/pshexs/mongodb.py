f6f2eb68f68c6b08cc3093287f24b94b5249fc9f
chadcarlson <chad.carlson@platform.sh>
2019-02-26 13:21:06 -0600

    update credentials output for debug

--- python/pshexs/mongodb.py
+++ python/pshexs/mongodb.py
@@ -15,44 +15,33 @@ def test_output():
 
     try:
 
-        # server = '{}://{}:{}@{}:{}/{}'.format(
-        #     credentials['scheme'],
-        #     credentials['username'],
-        #     credentials['password'],
-        #     credentials['host'],
-        #     credentials['port'],
-        #     credentials['path']
-        # )
-
-
         client = MongoClient(credentials['host'], credentials['port'])
         # client = MongoClient(credentials['host'], credentials['port'], credentials['username'], credentials['password'])
 
-        # client = MongoClient(server)
-
         db = client.test_database
 
-        # ERROR HERE - no users authenticated
-        db.command("createUser", credentials['username'], pwd=credentials['password'], roles=["root"])
+        # # ERROR HERE - no users authenticated
+        # db.command("createUser", credentials['username'], pwd=credentials['password'], roles=["root"])
+        #
+        # # db.createUser(credentials['username'], "readWrite")
+        #
+        # # db.grantRolesToUser(credentials['username'], ["readWrite"])
+        #
+        # # db.grantRolesToUser('admin', [{role: "root", db: "admin"}])
+        #
+        # collection = db.starwars
+        #
+        # rey = {"name": "Rey",
+        #        "occupation": "Jedi"}
+        #
+        # result_id = collection.insert(rey).inserted_id
+        #
+        # document = collection.find_one(
+        #     {"_id": result_id}
+        # )
 
-        # db.createUser(credentials['username'], "readWrite")
-
-        # db.grantRolesToUser(credentials['username'], ["readWrite"])
-
-        # db.grantRolesToUser('admin', [{role: "root", db: "admin"}])
-
-        collection = db.starwars
-
-        rey = {"name": "Rey",
-               "occupation": "Jedi"}
-
-        result_id = collection.insert(rey).inserted_id
-
-        document = collection.find_one(
-            {"_id": result_id}
-        )
-
-        return print('Found {0} ({1})<br />\n'.format(document.name, document.occupation))
+        # return print('Found {0} ({1})<br />\n'.format(document.name, document.occupation))
+        return credentials
 
     except Exception as e:
         return traceback.format_exc(), sys.exc_info()[0]
