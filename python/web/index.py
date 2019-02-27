47bf56c7b6e08b748d69dace09dd587ca91f9f4b
chadcarlson <chad.carlson@platform.sh>
2019-02-26 18:02:29 -0600

    subprocess.run

--- python/web/index.py
+++ python/web/index.py
@@ -47,7 +47,8 @@ def capture_output(example):
     # sys.stdout = old_stdout
 
     cmd = 'python3 ' + file
-    out = subprocess.check_output(cmd, shell=True)
+    # out = subprocess.check_output(cmd, shell=True)
+    out = subprocess.run(cmd, stdout=subprocess.PIPE)
 
     # file = os.getcwd() + '/examples/' + example + '.py'
     #
@@ -63,7 +64,7 @@ def capture_output(example):
     # # out = sys.stdout.getvalue()
     # out = backup.getvalue()
 
-    return out
+    return out.stdout
 
 
 @app.route('/python/<example>')
