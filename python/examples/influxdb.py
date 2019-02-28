edcd00f6da1e0a83898381d881726d884f752f3f
chadcarlson <chad.carlson@platform.sh>
2019-02-28 15:52:53 -0600

    remove add db

--- python/examples/influxdb.py
+++ python/examples/influxdb.py
@@ -50,8 +50,12 @@ def test_output():
 
         client = InfluxDBClient(host=credentials['ip'], port=credentials['port'], username=user, password=password)
 
-        dbname = 'deploys'
-        client.create_database(dbname)
+
+
+
+
+        # dbname = 'deploys'
+        # client.create_database(dbname)
 
 
         # client.query('SHOW DATABASES;')
@@ -123,7 +127,7 @@ def test_output():
         #     table += "</tbody>\n</table>\n"
         #
         # Drop the database.
-        client.drop_database(dbname)
+        # client.drop_database(dbname)
         #
         #
         # return table
