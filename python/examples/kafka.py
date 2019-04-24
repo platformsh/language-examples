49a59365029c4defd9eeba84f42c87937d684487
Ori Pekelman <ori@pekelman.com>
2019-04-24 16:16:50 +0200

    Correct example

--- python/examples/kafka.py
+++ python/examples/kafka.py
@@ -1,4 +1,3 @@
-
 from json import dumps
 from json import loads
 from kafka import KafkaConsumer, KafkaProducer
@@ -6,43 +5,37 @@ from platformshconfig import Config
 
 
 def usage_example():
-
     # Create a new Config object to ease reading the Platform.sh environment variables.
     # You can alternatively use os.environ yourself.
     config = Config()
-
     # Get the credentials to connect to the Kafka service.
     credentials = config.credentials('kafka')
-
+    
     try:
-
         kafka_server = '{}:{}'.format(credentials['host'], credentials['port'])
-
+        
         # Producer
         producer = KafkaProducer(bootstrap_servers=[kafka_server],
-                                 value_serializer=lambda x: dumps(x).encode('utf-8'))
+        value_serializer=lambda x: dumps(x).encode('utf-8'))
         for e in range(10):
             data = {'number' : e}
             producer.send('numtest', value=data)
-
+        
         # Consumer
         consumer = KafkaConsumer(
-            'numtest',
-            bootstrap_servers=[kafka_server],
-            auto_offset_reset='earliest',
-            enable_auto_commit=True,
-            group_id='my-group',
-            value_deserializer=lambda x: loads(x.decode('utf-8')))
-
+        bootstrap_servers=[kafka_server], 
+        auto_offset_reset='earliest')
+        
+        consumer.subscribe(['numtest'])
+        
         output = ''
-        message = next(consumer)
-        print(dir(message))
-
-        # for message in consumer:
-        #     print(dir(message))
-        #     #output += message.value.number
-
+        for message in consumer:
+            number = loads(message.value.decode('UTF-8'))["number"]
+            print (number)
+            output += number
+                
+        consumer.close()
         return output
-
+    
     except Exception as e:
         return e
