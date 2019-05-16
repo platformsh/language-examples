7ca7379449e3fa8f70d5596fccee8fe59f061c74
chadcarlson <chad.carlson@platform.sh>
2019-05-16 14:17:48 -0400

    Add drop functions for php and python

--- python/examples/mongodb.py
+++ python/examples/mongodb.py
@@ -37,6 +37,9 @@ def usage_example():
             {"_id": post_id}
         )
 
+        # Clean up after ourselves.
+        collection.drop()
+
         return 'Found {0} ({1})<br />'.format(document['name'], document['occupation'])
 
     except Exception as e:
