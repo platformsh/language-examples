1059f2d9741b95848282bd21a5741e1cbc6a3659
Otavio Santana <otaviopolianasantana@gmail.com>
2019-06-19 11:31:14 -0300

    improves java html presentation

--- java/src/main/java/sh/platform/languages/SampleCode.java
+++ java/src/main/java/sh/platform/languages/SampleCode.java
@@ -8,23 +8,16 @@ final class SampleCode {
 
     private final String source;
 
-    private final String htmlSource;
-
     private final Supplier<String> demoClass;
 
     SampleCode(SampleCodeType type, String source, Supplier<String> demoClass) {
         this.type = type;
         this.source = source;
-        this.htmlSource = source.replace(System.lineSeparator(), "<br/>");
         this.demoClass = demoClass;
     }
 
     public String getSource() {
         return source;
-    }
-
-    public String getHtmlSource() {
-        return htmlSource;
     }
 
     public String execute() {
