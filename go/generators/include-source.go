d96e233e0addecea7692eff00b603f69abf74eb0
Larry Garfield <larry@platform.sh>
2019-06-28 12:33:13 -0500

    Make the code generator multi-threaded.

--- go/generators/include-source.go
+++ go/generators/include-source.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 )
 
 const (
 	sourceDirectory = "examples"
 	backTick        = "\\x60"
-	outputFile = "sources.go"
+	outputFile      = "sources.go"
 )
 
 type debacktickWriter struct {
@@ -36,27 +36,32 @@ func main() {
 	if err != nil {
 		log.Fatal("Writing header failed: ", err)
 	}
+	var wg sync.WaitGroup
 	for _, f := range fs {
 		if strings.HasSuffix(f.Name(), ".go") {
-			_, err := out.Write([]byte(strings.TrimSuffix(f.Name(), ".go") + " = `"))
-			if err != nil {
-				log.Fatal("Writing line prefix failed: ", err)
-			}
-			file, err := os.Open(filepath.Join(sourceDirectory, f.Name()))
-			if err != nil {
-				log.Fatal("Opening file failed: ", err)
-			}
-			defer file.Close()
-			numBytes, err := io.Copy(outFilterBacktick, file)
-			fmt.Printf("Data read from %s: %d\n", f.Name(), numBytes)
-			if err != nil {
-				log.Fatal("Writing file content failed: ", err)
-			}
-			_, err = out.Write([]byte("`\n"))
-			if err != nil {
-				log.Fatal("Writing header failed", err)
-			}
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				_, err := out.Write([]byte(strings.TrimSuffix(f.Name(), ".go") + " = `"))
+				if err != nil {
+					log.Fatal("Writing line prefix failed: ", err)
+				}
+				file, err := os.Open(filepath.Join(sourceDirectory, f.Name()))
+				if err != nil {
+					log.Fatal("Opening file failed: ", err)
+				}
+				defer file.Close()
+				_, err = io.Copy(outFilterBacktick, file)
+				if err != nil {
+					log.Fatal("Writing file content failed: ", err)
+				}
+				_, err = out.Write([]byte("`\n"))
+				if err != nil {
+					log.Fatal("Writing header failed", err)
+				}
+			}()
 		}
 	}
+	wg.Wait()
 	_, _ = outFilterBacktick.Write([]byte(")\n"))
 }
