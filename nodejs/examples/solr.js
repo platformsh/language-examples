91ef9d02862e9dcd04a289c35c277219a4cf774d
Larry Garfield <larry@platform.sh>
2019-09-17 14:22:57 -0500

    Use unique variables for Node-Solr results.

--- nodejs/examples/solr.js
+++ nodejs/examples/solr.js
@@ -8,24 +8,24 @@ exports.usageExample = async function() {
     let output = '';
 
     // Add a document.
-    var result = await client.update({
+    let addResult = await client.update({
         id: 123,
         name: 'Valentina Tereshkova',
     });
 
-    output += "Adding one document. Status (0 is success): " + result.responseHeader.status +  "<br />\n";
+    output += "Adding one document. Status (0 is success): " + addResult.responseHeader.status +  "<br />\n";
 
     // Flush writes so that we can query against them.
     await client.softCommit();
 
     // Select one document:
     let strQuery = client.query().q();
-    result = await client.search(strQuery);
-    output += "Selecting documents (1 expected): " + result.response.numFound + "<br />\n";
+    let writeResult = await client.search(strQuery);
+    output += "Selecting documents (1 expected): " + writeResult.response.numFound + "<br />\n";
 
     // Delete one document.
-    result = await client.delete({id: 123});
-    output += "Deleting one document. Status (0 is success): " + result.responseHeader.status + "<br />\n";
+    let deleteResult = await client.delete({id: 123});
+    output += "Deleting one document. Status (0 is success): " + deleteResult.responseHeader.status + "<br />\n";
 
     return output;
 };
